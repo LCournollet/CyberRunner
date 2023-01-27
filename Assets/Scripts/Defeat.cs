14af69eb9cea400c04c2718f7346ca768ffa2327
HeroFactory16 <cedric.coinlag@gmail.com>
2023-01-27 16:09:04 +0100

    Conflict

--- Assets/Scripts/Defeat.cs
+++ Assets/Scripts/Defeat.cs
@@ -5,12 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Defeat : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public void RestartGame()
     {
         Debug.Log("Restarting game...");
         SceneManager.LoadScene(0);
     }
 
+=======
+    // Start is called before the first frame update
+
+    public void RestartGame()
+    {
+        Debug.Log("Restarting...");
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+>>>>>>> Stashed changes
     public void QuitGame()
     {
         Debug.Log("Exiting game...");
